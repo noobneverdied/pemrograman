8ab68ef0f184396083115b9a46a7925935e530ce
noobneverdied <132773305+noobneverdied@users.noreply.github.com>
2023-05-07 22:57:16 +0700

    Update 4_🧮_Kalkulator_TDS.py

--- pages/4_🧮_Kalkulator_TDS.py
+++ pages/4_🧮_Kalkulator_TDS.py
@@ -14,3 +14,7 @@ tombol = st.button('Hitung nilai TDS')
 if tombol:
     nilai_TDS = (B-A) / V * 1000
     st.success(f'Nilai TDS adalah {nilai_TDS}')
+    
+st.write('---')
+st.header('Baku Mutu Standar TDS')
+st.markdown("""Baku mutu standar TDS dapat bervariasi tergantung pada kebutuhan dan regulasi yang berlaku di suatu daerah atau negara. Namun, umumnya baku mutu standar TDS ditetapkan dalam satuan mg/L atau ppm (bagian per juta) dan berkisar antara 500-1000 mg/L untuk air minum dan berkisar antara 1000-3000 mg/L untuk air permukaan dan limbah industri""")
