6d78ad1d3e263c9b46b9b969d3b193ca81ad3e9f
noobneverdied <132773305+noobneverdied@users.noreply.github.com>
2023-05-08 00:05:51 +0700

    Update 5_🧮_Kalkulator_TSS.py

--- pages/5_🧮_Kalkulator_TSS.py
+++ pages/5_🧮_Kalkulator_TSS.py
@@ -5,7 +5,7 @@ st.title("""Perhitungan TSS""")
 st.write('---')
 st.title('TSS')
 
-A = st.number_input('Masukkan nilai berat kertas saring (g) atau berat cawan + kertas saring')
+A = st.number_input('Masukkan nilai berat kertas saring (mg) atau berat cawan + kertas saring')
 B = st.number_input('Masukkan nilai berat sampel pada kertas saring (g) atau berat cawan sampel + kertas saring residu setelah pemanasan')
 V = st.number_input('Masukkan nilai volume sampel (mL)')
 
