ee3c1256505702b7aad7b1401b8aa2eaa6dbdd22
noobneverdied <132773305+noobneverdied@users.noreply.github.com>
2023-05-07 18:41:50 +0700

    Update 4_🧮_Kalkulator_TDS.py

--- pages/4_🧮_Kalkulator_TDS.py
+++ pages/4_🧮_Kalkulator_TDS.py
@@ -5,12 +5,12 @@ st.title("""Perhitungan TDS""")
 st.write('---')
 st.title('TDS')
 
-A = st.number_input('Masukkan berat cawan (mg)')
-B = st.number_input('Masukkan nilai berat sampel pada cawan (mg)')
-V = st.number_input('Masukkan volume sampel (L)')
+A = st.number_input('Masukkan berat cawan (g)')
+B = st.number_input('Masukkan nilai berat sampel pada cawan (g)')
+V = st.number_input('Masukkan volume sampel (mL)')
 
 tombol = st.button('Hitung nilai TDS')
 
 if tombol:
-    nilai_TDS = ((B-A)/V)*(100/100)
+    nilai_TDS = ((B-A *1000)/(V/1000))
     st.success(f'Nilai TDS adalah {nilai_TDS}')
