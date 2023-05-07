14dc160ba71f859903dc3103bcbfc082d237adc2
noobneverdied <132773305+noobneverdied@users.noreply.github.com>
2023-05-07 22:58:44 +0700

    Update 6_🧮_Kalkulator_TS.py

--- pages/6_🧮_Kalkulator_TS.py
+++ pages/6_🧮_Kalkulator_TS.py
@@ -15,3 +15,8 @@ tombol = st.button('Hitung nilai TS')
 if tombol:
     nilai_TS = (A-B) / V * 1000
     st.success(f'Nilai TS adalah {nilai_TS}')
+    
+    
+st.write('---')
+st.header('Baku Mutu Standar TS')
+st.markdown("""Baku mutu standar TS dapat bervariasi tergantung pada kebutuhan dan regulasi yang berlaku di suatu daerah atau negara. Namun, umumnya baku mutu standar TS ditetapkan dalam satuan mg/L atau ppm (bagian per juta) dan berkisar antara 500-1000 mg/L untuk air permukaan dan 2000-4000 mg/L untuk limbah domestik dan industri.""")
