e7107bb14914ffe19da5e769aa34da9b69648d4f
noobneverdied <132773305+noobneverdied@users.noreply.github.com>
2023-05-07 18:43:06 +0700

    Update 6_🧮_Kalkulator_TS.py

--- pages/6_🧮_Kalkulator_TS.py
+++ pages/6_🧮_Kalkulator_TS.py
@@ -6,12 +6,12 @@ st.write('---')
 st.title('TS')
 
 
-A = st.number_input('Masukkan nilai berat cawan akhir + residu(mg)')
-B = st.number_input('Masukkan nilai berat cawan (mg)')
-V = st.number_input('Masukkan nilai volume (L)')
+A = st.number_input('Masukkan nilai berat cawan akhir + residu(g)')
+B = st.number_input('Masukkan nilai berat cawan (g)')
+V = st.number_input('Masukkan nilai volume (mL)')
 
 tombol = st.button('Hitung nilai TS')
 
 if tombol:
-    nilai_TS = ((A-B)/V)*(100/100)
+    nilai_TS = ((A-B*1000)/(v/1000)
     st.success(f'Nilai TS adalah {nilai_TS}')
